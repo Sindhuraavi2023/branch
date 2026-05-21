cd132bf668b23aadcaa5ecc32d8aa02a95216da5
Sindhuraavi2023 <sindhuraavi14@gmail.com>
2026-05-21 09:44:28 +0530

    Update New.java

--- New.java
+++ New.java
@@ -1,7 +1,8 @@
-public class New {
+public class Simple {
     public static void main(String[] args) {
         int a = 20;
         int b = 30;
+        int c = a + b;
         System.out.println(c);
     }
 }
